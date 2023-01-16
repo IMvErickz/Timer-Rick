1cee9d91fdb837592ac691cb46bab62765f74628
rick-silva <ricksant.1@hotmail.com>
2023-01-15 22:00:48 -0300

    add botao play

--- src/assets/components/Timer/timer.jsx
+++ src/assets/components/Timer/timer.jsx
@@ -1,30 +1,36 @@
 
 import React from "react";
+import { useState } from "react";
 
 const Timer = () =>{
 
-    let segundos = 59
+    // const [valor, setvalor] = useState
+
+    let segundos = 10
     let minutos = 1
     
-    const timer = setInterval( ()=>{
+    function timer(){ 
+        const cronometro = setInterval( ()=>{
 
-      console.log(`Minutos: ${minutos} Segundos: ${segundos}`)
-      
-      if(segundos <= 0 && minutos <= 0){
-        clearInterval(timer)
-      }else if(segundos > 0){
-        --segundos
-      }else if(segundos <= 0){
-        --minutos
-      }
-    },1000)
+        console.log(`Minutos: ${minutos} Segundos: ${segundos}`)
+        
+        if(segundos <= 0 && minutos <= 0){
+          clearInterval(cronometro)
+        }else if(segundos > 0){
+          --segundos
+        }else if(segundos <= 0){
+          --minutos
+        }
+      },1000)
+    }//fim function
+    
 
     return(
         <div>
             <h1>Timer</h1>
-
+            <input type="button" value="Play" onClick={timer} />
         </div>
     )
-}
+}//fim component
 
 export default Timer
