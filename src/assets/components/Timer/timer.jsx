2d2fa62b0bd92c021fc0bea8e3bfdacb486c8dc6
IMvErickz <erickspy2003@gmail.com>
2023-01-20 16:02:50 -0300

    added a input for change the time

--- src/assets/components/Timer/timer.jsx
+++ src/assets/components/Timer/timer.jsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 
 export const Timer = () =>{
 
-    const [segundosTotal, setSegundosTotal] = useState(0);
+  const [segundosTotal, setSegundosTotal] = useState(0);
+  console.log(segundosTotal)
     // const [minutos, setMinutos] = useState(0)
 
     const minutos = Math.floor(segundosTotal / 60)
@@ -34,7 +35,14 @@ export const Timer = () =>{
 
     return(
         <div className="w-screen h-screen  flex flex-col items-center justify-center">
-            <h1 className="text-white w- text-6xl font-bold">Timer</h1>
+           <div className="flex flex-col items-center justify-center gap-y-4">
+             <h1 className="text-white w- text-6xl font-bold">Timer</h1>
+          <input type="text"
+            placeholder="Coloque aqui quantos minutos você quer"
+            className="w-80 text-white bg-gray-800 rounded-lg p-2 placeholder:text-black text-center"
+            onChange={event => setSegundosTotal(event.target.value)}
+          />
+           </div>
             {/* padStart completa a string com '2' caracteres, caso não haja '2' complete com 0 */}
           <div className="w-full flex flex-row items-center justify-center p-3 text-white">
             <span className="text-3xl text-red-600">{ minutos.toString().padStart(2, "0") }</span>
